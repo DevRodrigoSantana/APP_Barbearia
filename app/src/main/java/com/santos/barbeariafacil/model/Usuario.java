67371746e8891cc454af8f18291725950cc4f6aa
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2024-01-03 21:44:53 -0300

    criação de cadastro de usuario

---
+++ app/src/main/java/com/santos/barbeariafacil/model/Usuario.java
@@ -0,0 +1,53 @@
+package com.santos.barbeariafacil.model;
+
+public class Usuario {
+    private String nomeCompleto;
+    private String email;
+    private String telefone;
+
+    private String senha;
+    private String confirmarSenha;
+
+    public Usuario() {
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfirmarSenha() {
+        return confirmarSenha;
+    }
+
+    public void setConfirmarSenha(String confirmarSenha) {
+        this.confirmarSenha = confirmarSenha;
+    }
+}
--- app/src/main/java/com/santos/barbeariafacil/view/Cadastro.java
+++ app/src/main/java/com/santos/barbeariafacil/view/Cadastro.java
@@ -1,16 +1,100 @@
 package com.santos.barbeariafacil.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.santos.barbeariafacil.R;
+import com.santos.barbeariafacil.config.ConfigFirebase;
 
 public class Cadastro extends AppCompatActivity {
+
+    private TextInputEditText campoNome, campoEmail, campoSenha, campoConfirmaSenha,campoTelefone;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        campoNome = findViewById(R.id.edit_nome);
+        campoEmail = findViewById(R.id.edit_email);
+        campoSenha = findViewById(R.id.edit_senha);
+        campoConfirmaSenha = findViewById(R.id.edit_confirma_senha);
+        campoTelefone = findViewById(R.id.edit_celular);
+
+    }
+    public void cadastroUsuario(){
+        autenticacao = ConfigFirebase.getFirebaseAutenticaca();
+        autenticacao.createUserWithEmailAndPassword("",""
+
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
+    }
+
+    public void validarCadastroUsuario(View view){
+
+        String textNome = campoNome.getText().toString();
+        String textEmail = campoEmail.getText().toString();
+        String textSenha = campoSenha.getText().toString();
+        String textConfirmaSenha = campoConfirmaSenha.getText().toString();
+        String textTelefone = campoTelefone.getText().toString();
+
+        if(!textNome.isEmpty()){
+            if(!textEmail.isEmpty()){
+                if (!textSenha.isEmpty()){
+                    if(!textConfirmaSenha.isEmpty()){
+                        if(!textTelefone.isEmpty()){
+                            if(textSenha  != textConfirmaSenha){
+
+                            }else {
+                                Toast.makeText(Cadastro.this,
+                                        "Preencha senhas são diferentes corretamente!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }else{
+                            Toast.makeText(Cadastro.this,
+                                    "Preencha o campo  telefone corretamente!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(Cadastro.this,
+                                "Preencha a senha de confirmação corretamente!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(Cadastro.this,
+                            "Preencha o senha corretamente!",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }else{
+                Toast.makeText(Cadastro.this,
+                        "Preencha o Email corretamente!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(Cadastro.this,
+                    "Preencha o nome corretamente!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
