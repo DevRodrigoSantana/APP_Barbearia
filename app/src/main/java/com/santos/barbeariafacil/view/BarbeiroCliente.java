d236e34b6f57981f5c5822a029cd0b8932fe75b8
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2024-01-12 13:03:26 -0300

    criacao de animacoes da activity de escolha

--- app/src/main/java/com/santos/barbeariafacil/view/BarbeiroCliente.java
+++ app/src/main/java/com/santos/barbeariafacil/view/BarbeiroCliente.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import com.santos.barbeariafacil.R;
@@ -12,6 +15,7 @@ import com.santos.barbeariafacil.R;
 public class BarbeiroCliente extends AppCompatActivity {
 
     private Button botaoBarbeiro, botaoCliente, botaoLogin;
+    private Animation animacaoEntrada, animacaoSaida,animacaoSaidaLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,36 @@ public class BarbeiroCliente extends AppCompatActivity {
         botaoBarbeiro = findViewById(R.id.button_barbeiro);
         botaoCliente = findViewById(R.id.button_cliente);
         botaoLogin = findViewById(R.id.buttonVoltarLogin);
+        animacaoEntrada = AnimationUtils.loadAnimation(this,R.anim.entrada_botao);
+        animacaoSaida=  AnimationUtils.loadAnimation(this,R.anim.saida_botao);
+        animacaoSaidaLeft = AnimationUtils.loadAnimation(this,R.anim.saida_left);
+
+        botaoBarbeiro.startAnimation(animacaoEntrada);
+        botaoCliente.startAnimation(animacaoEntrada);
+        botaoLogin.startAnimation(animacaoEntrada);
+
+        botaoBarbeiro.setVisibility(View.VISIBLE);
+        botaoCliente.setVisibility(View.VISIBLE);
+        botaoLogin.setVisibility(View.VISIBLE);
 
         botaoBarbeiro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openCadastroBarbeiro();
+
+                botaoCliente.startAnimation(animacaoSaida);
+                botaoLogin.startAnimation(animacaoSaidaLeft);
+                botaoCliente.setVisibility(View.INVISIBLE);
+                botaoLogin.setVisibility(View.INVISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+                        openCadastroBarbeiro();
+                    }
+                },400);
+
+
 
             }
         });
@@ -33,7 +62,21 @@ public class BarbeiroCliente extends AppCompatActivity {
         botaoCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openCadastroCliente();
+
+                botaoBarbeiro.startAnimation(animacaoSaida);
+                botaoLogin.startAnimation(animacaoSaidaLeft);
+
+                botaoBarbeiro.setVisibility(View.INVISIBLE);
+                botaoLogin.setVisibility(View.INVISIBLE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        openCadastroCliente();
+                    }
+                },400);
+
+
 
             }
         });
@@ -41,12 +84,53 @@ public class BarbeiroCliente extends AppCompatActivity {
         botaoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openLogin();
+                botaoBarbeiro.startAnimation(animacaoSaida);
+                botaoCliente.startAnimation(animacaoSaida);
+
+                botaoCliente.setVisibility(View.INVISIBLE);
+                botaoBarbeiro.setVisibility(View.INVISIBLE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        openLogin();
+                    }
+                },400);
+
+
 
             }
         });
 
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        botaoBarbeiro.startAnimation(animacaoEntrada);
+        botaoCliente.startAnimation(animacaoEntrada);
+        botaoLogin.startAnimation(animacaoEntrada);
+
+        botaoBarbeiro.setVisibility(View.VISIBLE);
+        botaoCliente.setVisibility(View.VISIBLE);
+        botaoLogin.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        botaoBarbeiro.startAnimation(animacaoEntrada);
+        botaoCliente.startAnimation(animacaoEntrada);
+        botaoLogin.startAnimation(animacaoEntrada);
+
+        botaoBarbeiro.setVisibility(View.VISIBLE);
+        botaoCliente.setVisibility(View.VISIBLE);
+        botaoLogin.setVisibility(View.VISIBLE);
+
+    }
+
     public void openLogin(){
         Intent intent = new Intent(BarbeiroCliente.this,Login.class);
         startActivity(intent);
@@ -56,12 +140,8 @@ public class BarbeiroCliente extends AppCompatActivity {
         startActivity(intent);
     }
     public void openCadastroBarbeiro(){
-        Intent intet = new Intent(BarbeiroCliente.this, CadastroBarbeiro.class);
-        startActivity(intet);
+        Intent intent = new Intent(BarbeiroCliente.this, CadastroBarbeiro.class);
+        startActivity(intent);
     }
-
-
-
-
 
 }
